34785dcd64b8b56242da7134a8cbe5c07851966e
Alexander Murakhtin <alexander.murakhtin@metatelematics.com>
2026-08-02 10:37:59 +0400

    api url name read fix

--- client/src/drawing.ts
+++ client/src/drawing.ts
@@ -19,7 +19,7 @@ interface Elements {
 
 // ---------- Config ----------
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 const DOT_RADIUS = Number(import.meta.env.VITE_DOT_RADIUS);
 const POINTS_SEND_INTERVAL_MS = Number(import.meta.env.VITE_POINTS_SEND_INTERVAL_MS);
 const POINTER_SEND_INTERVAL_MS = Number(import.meta.env.VITE_POINTER_SEND_INTERVAL_MS);
